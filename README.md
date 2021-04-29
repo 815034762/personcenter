9982d542de1804525c37de6fa5edb3534d88cd99
zhangty <zhangty@szlanyou.com>
2021-04-29 21:37:35 +0800

    个人中心代码提交

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# personcenter
-组件化个人中心模块
+# AndroidFrame
+用于Android学习使用，主要用到jetpack相关的内容，LiveData,DataBinding，paging，MVVM,RXJAVA,Retrofit等等，后面会慢慢增加更多的内容
---
+++ app/src/main/java/com/example/personcenter/PersonMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.personcenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_main);
+    }
+}
